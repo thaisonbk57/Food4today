44964093a8a973af34cea6543c814712eb500f1e
Thai Son Dang <thaisondang.bk57@gmail.com>
2019-01-11 01:33:38 +0100

    injected GlobalStyle with styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*, *::after, *::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`;
+
+const Wrapper = styled.div`
+  background: orangered;
+  font-size: 60px;
+  color: papayawhip;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h3>App component</h3>
-      </div>
+      <Wrapper>
+        <GlobalStyle />
+      </Wrapper>
     );
   }
 }
