4105f047766ecfc177b4ee2af4f5317a134e5f45
Thai Son Dang <thaisondang.bk57@gmail.com>
2019-01-12 03:39:16 +0100

    added Loader UI cmp

---
+++ src/components/UI/Loader/Loader.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+// Create the keyframes
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  } to {
+    transform: rotate(360deg);
+  }
+`;
+
+export default function(props) {
+  const Loader = styled.div`
+    border: ${props.borderWidth || 8}px solid #f3f3f3;
+    border-radius: 50%;
+    border-top: ${props.borderWidth || 8}px solid #3498db;
+    width: ${props.width || 50}px;
+    height: ${props.width || 50}px;
+    animation: ${rotate} 2s linear infinite;
+  `;
+  return <Loader />;
+}
